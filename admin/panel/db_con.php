69d8893b721a0ecb728642c02ca5efccd62d74fa
Muhammad Azeem <malikazeempk@hotmail.com>
2017-09-26 01:11:58 +0500

    Update db_con.php

--- admin/panel/db_con.php
+++ admin/panel/db_con.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $link 				= mysql_connect('localhost', 'root', '');
 	if (!$link) {
 	    die('Not connected : ' . mysql_error());
@@ -8,7 +8,7 @@ $db_selected 	= mysql_select_db('digital_word', $link);
 	if (!$db_selected) {
 	    die ('Can\'t use foo : ' . mysql_error());
 	}
-*/
+
 
 
 
